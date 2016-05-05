05d69a2049ee9256ff095f152f8323e3942ed1b9
zydar16 <maxime.venaud@gmx.fr>
2016-05-05 21:38:53 +0200

    Add FR locale

---
+++ fr.php
@@ -0,0 +1,8 @@
+<?php
+$Definition['Plugins.CookieConsent.MessageText'] = 'Essai du message';
+$Definition['Plugins.CookieConsent.DismissText'] = 'Texte à accepter';
+$Definition['Plugins.CookieConsent.LearnMoreText'] = 'Texte pour plus d\'informations';
+$Definition['Plugins.CookieConsent.Theme'] = 'Bannière thème';
+$Definition['Plugins.CookieConsent.Message'] = 'Ce site utilise des cookies pour assurer la meilleure expérience sur notre site';
+$Definition['Plugins.CookieConsent.Dismiss'] = 'Je comprends!';
+$Definition['Plugins.CookieConsent.LearnMore'] = 'En savoir plus!';
